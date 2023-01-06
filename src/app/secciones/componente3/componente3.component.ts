eed0c8cdbff52cd08ced5f5d02eebd990194d035
MariCarolV9 <118744765+MariCarolV9@users.noreply.github.com>
2023-01-06 16:11:00 -0500

    Update componente3.component.ts

--- src/app/secciones/componente3/componente3.component.ts
+++ src/app/secciones/componente3/componente3.component.ts
@@ -6,16 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./componente3.component.css']
 })
 export class Componente3Component implements OnInit {
+
 datos = new Array;
 
 tarea = "";
 descripcion = "";
+resultado = false;
+mensaje = '';
 
 AgregarTarea(){
-  this.datos.push({'Tarea': this.tarea, 'Descripcion':this.descripcion});
-  console.log(this.datos);
+  if(this.tarea!='' &&this.descripcion!=''){
+    this.datos.push({'Tarea': this.tarea, 'Descripcion':this.descripcion});
+    this.tarea = "";
+    this.descripcion = "";
+    this.resultado = true;
+    this.mensaje="";
+  }else{
+    this.resultado = false;
+    this.mensaje="Ingresa los campos, para agregar a la lista";
+ }
 }
 
+
+Eliminar(indice:number){
+  this.datos.splice(indice, 1)
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
